6552be3e38ea166fe5e598a957cea6e769d0f8fd
NastyaNi <73160176+NastyaNi@users.noreply.github.com>
2021-10-21 00:47:49 +0300

    Create SimpleEditorListener.java

---
+++ SimpleEditorListener.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simpleeditor;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+
+/**
+ *
+ * @author vitya
+ */
+class SimpleEditorListener extends WindowAdapter implements ActionListener, AutoCloseable{
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void close() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
